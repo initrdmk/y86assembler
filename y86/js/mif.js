9a8fe74f2af497db5bbbd4e25acbe43e55e1d746
mingkaidox <mingkaidong@gmail.com>
2014-05-15 10:33:58 +0800

    add mif support

---
+++ y86/js/mif.js
@@ -0,0 +1,26 @@
+// y86 asm to quartus MIF file
+//  By mingkaidox
+
+var to_mif = function(codes, width, depth) {
+    if (0 != width % 8) return '';
+    width /= 4;
+    var out = "% MIF made by Y86 assembler\n";
+    out += "DEPTH = " + depth + ';\n';
+    out += "WIDTH = " + width + ';\n';
+    out += 'ADDRESS_RADIX = HEX;\n';
+    out += 'DATA_RADIX = HEX;\n';
+    out += 'CONTENT\n';
+    out += 'BEGIN\n';
+    var line = 0;
+    codes = codes.split(/\s+|\n/).join('');
+    for (var i=1; i<width; ++i) {
+        codes += '0';
+    }
+    while (codes.length >= width) {
+        out += ' ' + line + ' : ' + codes.slice(0, width) + ';\n';
+        line++;
+        codes = codes.slice(width);
+    }
+    out += 'END;\n';
+    return out;
+};
